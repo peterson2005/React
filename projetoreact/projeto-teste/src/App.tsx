80e9d170bc79623667b3657ec52d8c1ce92c6481
peterson honorato <petersonphf@gmail.com>
2025-11-25 16:55:14 -0300

    terminando exerc react

--- projetoreact/projeto-teste/src/App.tsx
+++ projetoreact/projeto-teste/src/App.tsx
@@ -1,11 +1,36 @@
 
+
+
+
+
+
+
+
+
+
+import { useState } from 'react';
 export function App() {
-  const [count, setCount]  = useState(0)
+  const [count, setCount] = useState(0);
+  let count2 = 0;
+  setInterval(() => {
+    console.log('count', count)
+    console.log('count2', count2)
+  }, 500);
+
 
   return (
-    <>
-    ola
-    </>
+
+    <div>
+      <button onClick={() => setCount(count + 1)}>
+        {count}
+      </button>
+      <button onClick={() => setCount(count2 = count2 + 1)}>
+        {count2}
+      </button>
+    </div>
+
   )
 }
+
+
 
