ec44ecbd49f6126ff9653498e7e6c1418337fe32
PETERSON HONORATO <petersonphf@gmail.com>
2026-01-31 20:41:23 -0300

    initial commit

--- projeto-DevClub/humor-tracker/App.tsx
+++ projeto-DevClub/humor-tracker/App.tsx
@@ -1,15 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [counter, setCounter] = useState(0);
-
   return (
     <View style={styles.container}>
+      <Text>Open up App.tsx to uaiii!</Text>
       <StatusBar style="auto" />
-      <Text>Não acredito Hello world!</Text>
-    
     </View>
   );
 }
@@ -19,6 +15,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justify: 'center',
+    justifyContent: 'center',
   },
 });
