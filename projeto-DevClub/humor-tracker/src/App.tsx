1e8c741397e53dd414bdbd711b317709e7e76697
peterson honorato <petersonphf@gmail.com>
2026-02-23 16:46:30 -0300

    quase nada

--- projeto-DevClub/humor-tracker/src/App.tsx
+++ projeto-DevClub/humor-tracker/src/App.tsx
@@ -1,30 +1,35 @@
-
-import { SafeAreaView } from 'react-native-safe-area-context';
-
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'; // Adicionei o SafeAreaProvider
 import { MyButton } from "./shared/components/MyButton";
-import { Text } from 'react-native';
+import { Text, StyleSheet, Pressable } from 'react-native';
 
 export default function App() {
-  return (<SafeAreaView>
-
-    <MyButton order={1} onPress={() => console.log('Teste')}>
-      <Text>MyButon {1} </Text>
-    </MyButton>
-    
-    <MyButton order={2} onPress={() => console.log('Teste')}>
-      <Text>MyButon {2} </Text>
-    </MyButton>
-    
-    <MyButton order={3} onPress={() => console.log('Teste')}>
-      <Text>MyButon {3} </Text>
-    </MyButton>
-
-    <MyButton order={4} onPress={() => console.log('Teste')}>
-      <Text>MyButon {4} </Text>
-    </MyButton>
+  return (
+    /* 1. O Provider deve envolver toda a aplicação */
+    <SafeAreaProvider>
+      {/* 2. O SafeAreaView agora vai funcionar corretamente */}
+      <SafeAreaView style={styles.container}>
 
 
-  </SafeAreaView>
+      <Pressable style={styles.button}>        
+        <Text>Press me</Text>
+      </Pressable>
+        
+
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
+// Adicionei um estilo básico para garantir que o app ocupe a tela toda
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: 'red',
+    width: 100, 
+    padding: 8,
+    paddingHorizontal: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 4,
+  }
+  },
+);
